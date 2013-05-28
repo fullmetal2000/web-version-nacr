06c00a9de99e9a392aec7b1a1fa443d12fbcaad8
fullmetal2000 <cheng.wangshu@gmail.com>
2013-05-27 22:24:10 -0700

    Web version nacr
    Completed CSS

---
+++ js/buildpages.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: wangshucheng
+ * Date: 13-05-12
+ * Time: 12:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ js/getdata.js
@@ -0,0 +1,15 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: wangshucheng
+ * Date: 13-05-12
+ * Time: 12:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+var URLInfo;
+URLInfo = {
+    baseurl:'',
+    keywords:'',
+    value:''
+
+
+};
